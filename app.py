3afef25c3d97695e54f05cb89b8aa366ecaabc3b
Bahey el Din Ismail <90637888+Bahey1200022@users.noreply.github.com>
2023-05-06 08:51:37 +0300

    oop nenenenene

--- app.py
+++ app.py
@@ -4,81 +4,113 @@ import cv2
 import base64
 import os
 
-def removefile(name):
-    file_path = name
-    if os.path.exists(file_path):
-        os.remove(name)
+class processimage:
     
-def processimage(matrix):
-    f = np.fft.fft2(matrix)
-    fshift = np.fft.fftshift(f)
-    magnitude_spectrum = 20*np.log(np.abs(fshift))
-    phase = np.angle(fshift)
-    realpart=np.real(fshift)
-    imgpart=np.imag(fshift)
-    return magnitude_spectrum,phase,realpart,imgpart
+    def processimage(self,matrix):
+        f = np.fft.fft2(matrix)
+        fshift = np.fft.fftshift(f)
+        magnitude_spectrum = 20*np.log(np.abs(fshift))
+        phase = np.angle(fshift)
+        realpart=np.real(fshift)
+        imgpart=np.imag(fshift)
+        components=[]
+        components.append(magnitude_spectrum)
+        components.append(phase)
+        components.append(realpart)
+        components.append(imgpart)
+        return components
+    ################################################################################################################################
+    ################################################################################################################################
+    def decodefromjs(self,data_url):
+        image_data = data_url.split(',')[1]
+
+    # Decode the image data from base64
+        decoded_data = base64.b64decode(image_data)
+    
+    # Convert the decoded data to a NumPy array
+        np_data = np.frombuffer(decoded_data, np.uint8)
+        return np_data
+    ################################################################################################################################
+    ################################################################################################################################    
+    def generate_image(self,filen,matrix1):
+        self.removefiles(filen)
+        print(type(matrix1))
+        cv2.imwrite(filen,matrix1)
+        filename=filen
+        return filename
+    ###################################################################################################################################################################################    
+    ################################################################################################## 
+    def generate_component(self,filen,matrix,indexofcomponent):
+        self.removefiles(filen)
+        wantedcomponent=Image.processimage(matrix)[indexofcomponent]
+        cv2.imwrite(filen,wantedcomponent)
+        filename=filen
+        return filename
+
+    def removefiles(self,name):
+        file_path = name
+        if os.path.exists(file_path):
+            os.remove(name)
     
 
+###################################################################################################################################################################################
+ ###################################################################################################################################################################################
 app = Flask(__name__)
 @app.route('/')
 def image_mixer():
-    return render_template('index.html')
+    global Image
+    Image=processimage()
 
+    return render_template('index.html')
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Get the data URL from the request JSON
-    data_url = request.json['image_data']
-    image_data = data_url.split(',')[1]
-
-# Decode the image data from base64
-    decoded_data = base64.b64decode(image_data)
- 
-# Convert the decoded data to a NumPy array
-    np_data = np.frombuffer(decoded_data, np.uint8)
+    data_url1 = request.json['image_data']
     global image1
-    image1 = cv2.imdecode(np_data, cv2.IMREAD_COLOR)
-
-    # f = np.fft.fft2(image1)
-    # global fshift
-    # fshift = np.fft.fftshift(f)
-    
-    # # magnitude_spectrum = 20*np.log(np.abs(fshift))
-    # # phase = np.angle(fshift)
-    # # realpart=np.real(fshift)
-    # # imgpart=np.imag(fshift)
-
+    image1 = cv2.imdecode(Image.decodefromjs(data_url1), cv2.IMREAD_COLOR)
     return 'Image saved!'
 
 @app.route('/image1')
 def image1():
-    removefile("image1.png")
-    cv2.imwrite("image1.png",image1)
-    filename="image1.png"
+    filename=Image.generate_image("image1.png",image1)
     return send_file(filename, mimetype='image/png')
 
     
 @app.route('/real1')
 def real():
-    removefile("real1.png")
-    magnitude_spectrum,phase,realpart,imgpart=processimage(image1)
-    cv2.imwrite('real1.png', realpart)
-    filename="real1.png"
+    filename=Image.generate_component("component1.png",image1,2)
     return send_file(filename, mimetype='image/png')
 
 @app.route('/imag1')
 def imaginary():
-    removefile("imag1.png")
-    magnitude_spectrum,phase,realpart,imgpart=processimage(image1)
-    cv2.imwrite('imag1.png', imgpart)
-    filename="imag1.png"
+    filename=Image.generate_component("component1.png",image1,3)
     return send_file(filename, mimetype='image/png')
     
-    
+@app.route('/phase1')
+def phase():
+    filename=Image.generate_component("component1.png",image1,1)
+    return send_file(filename, mimetype='image/png')    
 
+@app.route('/magnitude1')
+def mag1():
+    filename=Image.generate_component("component1.png",image1,0)
+    return send_file(filename, mimetype='image/png') 
 
+################################################################################################################
+################################################################################################################
+@app.route('/upload2', methods=['POST'])
+def upload2():
+    data_url = request.json['image_data']
+    global image2
+    image2 = cv2.imdecode(Image.decodefromjs(data_url), cv2.IMREAD_COLOR)
+    return 'Image saved!'
     
+@app.route('/image2')
+def image2():
+    filename=Image.generate_image("image2.png",image2)
+    return send_file(filename, mimetype='image/png')    
+
 if __name__ == '__main__':
     app.run(debug=True)
     
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,18 @@
 
 
 var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
 
 var div1 = document.getElementById('originalimg1');
 var div2 = document.getElementById('modified1');
-var div3 = document.getElementById('test1');
+
+var div3 = document.getElementById('originalimg2');
+var div4 = document.getElementById('modified2');
 
 img1.addEventListener('change', function(event) {
   // Get the selected file
   div1.setAttribute('src', '');
   div2.setAttribute('src', '');
-  div3.setAttribute('src', '');
 
   var file = event.target.files[0];
 
@@ -24,8 +26,6 @@ img1.addEventListener('change', function(event) {
     xhr.send(JSON.stringify({image_data: event.target.result}));
 
     div1.setAttribute("src",'/image1?' +  new Date().getTime());
-    div2.setAttribute("src",'/real1?' +  new Date().getTime());
-    div3.setAttribute("src",'/imag1?' +  new Date().getTime());
 
 
   };
@@ -33,3 +33,48 @@ img1.addEventListener('change', function(event) {
 
 });
 
+function showcomponent() {
+  var dropdown1 = document.getElementById("component");
+var selectedOption = dropdown1.options[dropdown1.selectedIndex].value;
+  if (selectedOption === "imag") {
+    div2.setAttribute('src', '');
+    div2.setAttribute("src",'/imag1?' +  new Date().getTime());
+
+  } else if (selectedOption === "real") {
+    div2.setAttribute('src', '');
+    div2.setAttribute("src",'/real1?' +  new Date().getTime());
+
+  } else if (selectedOption === "phase") {
+    div2.setAttribute('src', '');
+    div2.setAttribute("src",'/phase1?' +  new Date().getTime());
+
+  } else if (selectedOption ==="magnitude"){
+    div2.setAttribute('src', '');
+    div2.setAttribute("src",'/magnitude1?' +  new Date().getTime());
+
+  }
+}
+
+img2.addEventListener('change', function(event) {
+  // Get the selected file
+  div3.setAttribute('src', '');
+  div4.setAttribute('src', '');
+
+  var file = event.target.files[0];
+
+  // Read the file as a data URL
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    // Send the data URL to the Flask server using an AJAX request
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload2', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({image_data: event.target.result}));
+
+    div1.setAttribute("src",'/image1?' +  new Date().getTime());
+
+
+  };
+  reader.readAsDataURL(file);
+
+});
